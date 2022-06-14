76ecbe5829a5aa3f4ee5f9d7ce32040d19de56dd
taitran <taitran.apps@gmail.com>
2022-06-15 06:32:21 +0700

    declarative approach

---
+++ FPwithKotlin/src/main/kotlin/chappter1/DeclarativeSum1.kt
@@ -0,0 +1,17 @@
+package chappter1
+
+private val input = listOf("123", "ABC", "1ds","987", "adef","88", "101")
+private fun declarativeSum(list: List<String>):Int = list.filter(::isValidNumber).map(String::toInt).sum()
+
+private fun isValidNumber(str:String):Boolean{
+    try {
+        str.toInt()
+        return true
+    }catch (nfe : NumberFormatException){
+        return false
+    }
+}
+
+fun main(){
+    println("Sum ${declarativeSum(input)}")
+}
---
+++ FPwithKotlin/src/main/kotlin/chappter1/HigherOrder.kt
@@ -0,0 +1,11 @@
+package chappter1
+
+private fun Int.times(fn :()->Unit){
+    for (i in 1..this){
+        fn()
+    }
+}
+
+fun main(){
+    3.times({println("Hello World !!!")})
+}
