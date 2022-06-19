e309c1381df1d6b88622b11367fb11ec946e40da
Tai Tran <taitran.apps@gmail.com>
2022-06-20 06:45:36 +0700

    chapter8 bak

--- FPwithKotlin/src/main/kotlin/chapter1/Composition.kt
+++ FPwithKotlin/src/main/kotlin/chapter1/Composition.kt
@@ -1,10 +1,22 @@
 package chapter1
 
+import Fun
+import chapter2.after
+
 fun double(x:Int):Int = 2*x
 fun square(x: Int):Int = x*x
 fun squareAndDouble(x:Int) = double(square(x))
 
+
+inline infix fun <A,B,C> Fun<A,B>.compose(crossinline g: Fun<B,C>):Fun<A,C> = { a: A -> g(this(a))}
 fun main(){
-    val result = squareAndDouble(10)
-    println(result)
+//    val result = squareAndDouble(10)
+//    println(result)
+    val double = {a : Int -> a*2}
+    val square = {a:Int -> a*a}
+    val stringify = Int::toString
+    val stringifyDoubleSquareAfter = stringify after square after double
+    println(stringifyDoubleSquareAfter(2))
+    val stringifyDoubleSquareCompose = double compose square compose stringify
+    println(stringifyDoubleSquareCompose(2))
 }
---
+++ FPwithKotlin/src/main/kotlin/chapter8/Curry.kt
@@ -0,0 +1,37 @@
+package chapter8
+
+import chapter1.compose
+import chapter1.square
+import chapter4.double
+
+fun sum(a:Int):(Int)->Int = {b:Int-> a+b}
+typealias Fun2<A, B,C> = (A,B) -> C
+val sum = {a:Int, b: Int -> a+b}
+val double:(Int) -> Int = {a:Int->2*a}
+val square:(Int) -> Int = {a:Int->a*a}
+val stringify: (Int) -> String = {a:Int->a.toString()}
+
+fun <A, B,C> Fun2<A,B,C>.curry(): (A)->(B)->C={
+    a:A->{
+        b:B->this(a,b)
+}
+}
+
+fun comp(a:Int, b:Int):String {
+    val currySum: (Int) -> (Int) -> Int = sum.curry()
+    val doubleComposeSum: (Int)->(Int)->Int = double compose currySum
+    val right: (Int) -> Int = doubleComposeSum(a)
+    return (square compose right compose stringify)(b)
+}
+
+
+fun comp1(a: Int, b:Int):String{
+    val right = (double compose sum.curry())(a)
+    return (square compose right compose  stringify)(b)
+}
+fun main() {
+    val curriedSum = sum.curry()
+    val addThree = curriedSum(3)
+    val result = addThree(4)
+    println(comp(10,2))
+}
